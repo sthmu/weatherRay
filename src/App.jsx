20e4773561d199521399d210bb763f4ef11f8fd0
Limit Breaker <sthmudara@gmail.com>
2024-02-28 14:23:50 +0530

    initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+
+import './App.css'
+import WeatherSlide from './WeatherSlide'
+
+function App() {
+ 
+  return (
+    <>
+      
+      <h1>Whether Api Test</h1>
+
+    
+      <WeatherSlide></WeatherSlide>
+       
+
+     
+    </>
+  )
+}
+
+export default App
