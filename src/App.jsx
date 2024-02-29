a1151287830b6892d80beafd317ca85388394048
Limit Breaker <sthmudara@gmail.com>
2024-02-29 19:09:28 +0530

    api intergrated and display data

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,51 @@ import background from "./assets/Designer.jpeg";
 import "./App.css";
 import WeatherSlide from "./components/WeatherSlide";
 import SearchComponent from "./components/searchComponent";
+import { useEffect, useState } from "react";
+import apiKey from "./config/config";
+import axios from "axios";
 
 function App() {
+   const [weather  ,setWeather]= useState(null);
+
+
+   const fetchWeather=async ()=>{
+    
+   }
+
+  useEffect(() => {
+    ;(async () => {
+      const result = await axios.get(
+        "http://api.weatherapi.com/v1/current.json?key=" +
+          apiKey +
+          "&q=baduraliya"
+      );
+      console.log(result.data);
+
+      setWeather(result.data);
+    })();
+  }, [1]);
+
+
+
   return (
     <div
-      className="p-5 w-100  "
+      className="p-5"
       style={{
         backgroundImage: `url(${background})`,
         backgroundRepeat: "no-repeat",
         backgroundSize: "100% 100%",
-        minHeight: '100vh'
+        minHeight: "100vh",
       }}
     >
-      <div className="title w-75 rounded mx-auto p-5">
-      <h1 className="text-light ">Whether Api Test</h1>
-      <SearchComponent></SearchComponent>
+      <div className="row">
+        <div className="title col col-12 col-lg-8 rounded mx-auto p-5">
+          <h1 className="text-light ">Whether Api Test</h1>
+          <SearchComponent></SearchComponent>
 
-      <WeatherSlide></WeatherSlide>
-
+          {weather && <WeatherSlide weather={weather} />}
+        </div>
       </div>
-
-      
-      
     </div>
   );
 }
--- src/components/WeatherSlide.jsx
+++ src/components/WeatherSlide.jsx
@@ -1,33 +1,28 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-
-
-export default function WeatherSlide() {
-  const [currentWeather, setCurrentWeather] = useState("current Whether");
-
-  useEffect(() => {
-    (async ()=>{
-      const result= await axios.get("https://catfact.ninja/fact")
-      console.log(result)
-    })()
-    
-  }, []);
-
-  
+export default function WeatherSlide(props) {
+  const temperature = props.weather.current?.temp_c;
+  const wind = props.weather.current?.wind_kph;
+  const condition = props.weather.current?.condition.text;
+  const humidity = props.weather.current?.humidity;
 
   return (
     <>
-
-      <div className="row">
-
-        <div className="col col-10 h-75 ">
-          
+      <div className="row text-light title rounded p-5">
+        <div className="col col-lg-3 h5 col-6 pb-5 ">
+        <h6>Temperature</h6> <br /> {temperature} c
         </div>
-
+        <div className="col col-lg-3 h5 col-6 pb-5">
+          <h6>Wind Speed</h6> <br /> {wind} Kph
+        </div>
+        <div className="col col-lg-3 h5 col-6 pb-5">
+          <h6>Condition</h6> <br /> {condition}
+        </div>
+        <div className="col col-lg-3 h5 col-6 pb-5">
+          <h6>Humidity</h6> <br /> {humidity}
+        </div>
       </div>
-        
- 
     </>
   );
 }
